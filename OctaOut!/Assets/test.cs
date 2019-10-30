d3fd8cc729db749e5eb4c3d583de90e886a4521e
friak <friak42@gmail.com>
2019-10-30 16:02:58 -0600

    Moved git files on computer

---
+++ OctaOut!/Assets/test.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+public class test : MonoBehaviour
+{
+
+    private bool jsUp = false;
+    private bool jsDown = false;
+
+    //frame states
+    private string frameState = "nobend"; //player is idle
+    private float timeOut = 0f; //time within each state
+
+    //...sprites
+    public Sprite nobend;
+    public Sprite bend;
+
+    private SpriteRenderer spriteRenderer;
+
+
+    //Serial Communication
+    SerialPort sp = new SerialPort("COM4", 9600);
+
+
+    void Start()
+    {
+        // sp.Open() will throw an error if it can't find an arduino and this "try" block will catch that error and allow the game to keep running
+        try
+        {
+            sp.Open();
+            sp.ReadTimeout = 25;
+        }
+        catch (System.Exception)
+        {
+            Debug.Log("Controller Not Found!");
+        }
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer.sprite == null) // if the sprite on spriteRenderer is null then
+            spriteRenderer.sprite = nobend;
+    }
+
+    void Update()
+    {
+
+        //SERIAL
+        if (sp.IsOpen)
+        {
+            try
+            {
+                MoveObject(sp.ReadByte());
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+
+
+
+    }
+
+
+
+
+    //DEBUG
+    void MoveObject(int data)
+    {
+
+        if (data == 1) //up
+        {
+            Debug.Log("bend");
+            spriteRenderer.sprite = bend;
+        }
+        else if (data == 2)
+        {
+            Debug.Log("nope");
+            spriteRenderer.sprite = nobend;
+        }
+
+    }
+ }
---
+++ arduino_code/test_code/test_code.ino
@@ -0,0 +1,67 @@
+#include <Wire.h>
+
+
+const int sampleWindow = 50; // Sample window width in milliseconds
+int bendPin = A0;
+int ledPin = 9;
+int bendval;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(bendPin, INPUT);
+
+}
+void loop() {
+
+  unsigned long start = millis(); // Start of sample window
+  unsigned int peakToPeak = 0;   // peak-to-peak level
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 1024;
+
+  // collect data for 50 miliseconds
+  while (millis() - start < sampleWindow)
+  {
+    bendval = analogRead(bendPin);
+    if (bendval < 1024)  //This is the max of the 10-bit ADC so this loop will include all readings
+    {
+      if (bendval > signalMax)
+      {
+        signalMax = bendval;  // save just the max levels
+      }
+      else if (bendval < signalMin)
+      {
+        signalMin = bendval;  // save just the min levels
+      }
+    }
+  }
+  peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+  double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
+
+
+  //Mic troubleshoot with LED
+  //Serial.println(volts);
+  if (volts >= 1.0)
+  {
+    digitalWrite(ledPin, HIGH);
+    delay(50);
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+  }
+  if (bendval >= 1000)
+  {
+    Serial.write(1); 
+    Serial.flush();
+    delay (20);
+  }
+  else 
+  {
+    Serial.write(2);
+    Serial.flush();
+    delay(20);
+  }
+    
+     // Serial.println(bendval);
+}
